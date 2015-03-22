c727b66de808ab967bd2ee8a7a484c54dc794a67
Sean Callahan <sean@seancallahan.net>
2015-03-21 23:09:14 -0700

    Changed login username regex and changed from tabs to spaces

--- src/main/java/org/starmod/net/handler/LoginHandler.java
+++ src/main/java/org/starmod/net/handler/LoginHandler.java
@@ -6,32 +6,24 @@ import org.starmod.net.command.Login;
 
 public class LoginHandler implements Handler<Login> {
 
-	@Override
-	public void handle(NetworkClient modClient, Login cmd) {
-		int returnCode;
-		String playerName = cmd.getPlayerName();
-		System.out.println("[StarMod][Network][Login] Client " + modClient.getNetworkId() + " attempting to login with username: " + playerName);
-		if (playerName.length() <= 0) {
-		    returnCode = Login.LoginCode.ERROR_INVALID_USERNAME.getCode();
-		} else if (playerName.length() > 32) {
-		    returnCode = Login.LoginCode.ERROR_INVALID_USERNAME.getCode();
-		} else if (playerName.length() <= 2) {
-		    returnCode = Login.LoginCode.ERROR_INVALID_USERNAME.getCode();
-		} else if (playerName.matches("[_-]+")) {
-		    returnCode = Login.LoginCode.ERROR_INVALID_USERNAME.getCode();
-		} else if (!playerName.matches("[a-zA-Z0-9_-]+")) {
-		    returnCode = Login.LoginCode.ERROR_INVALID_USERNAME.getCode();
-		} else {
-		    returnCode = Login.LoginCode.SUCCESS_LOGGED_IN.getCode();
-		}
-		if (returnCode != Login.LoginCode.SUCCESS_LOGGED_IN.getCode()) {
-		    System.out.println("[StarMod][Network][Login] Invalid login for Client " + modClient.getNetworkId() + " (" + Login.LoginCode.getById(returnCode).getMessage() + ")");
-		} else {
-		    System.out.println("[StarMod][Network][Login] Logging in Client " + modClient.getNetworkId() + " with protected username: " + playerName);
-		}
-		Login returnLogin = new Login(cmd.getHeader(), playerName, cmd.getVersion(), cmd.getAddress(), cmd.getLoginCode(), returnCode, cmd.getUserAgent(), modClient.getNetworkId());
-		modClient.send(returnLogin);
-		modClient.onLogin(cmd);
-	}
+    @Override
+    public void handle(NetworkClient client, Login cmd) {
+        int returnCode;
+        String playerName = cmd.getPlayerName();
+        System.out.println("[StarMod][Network][Login] Client " + client.getNetworkId() + " attempting to login with username: " + playerName);
+        if (!playerName.matches("[a-zA-Z0-9_-]{3,32}")) {
+            returnCode = Login.LoginCode.ERROR_INVALID_USERNAME.getCode();
+        } else {
+            returnCode = Login.LoginCode.SUCCESS_LOGGED_IN.getCode();
+        }
+        if (returnCode != Login.LoginCode.SUCCESS_LOGGED_IN.getCode()) {
+            System.out.println("[StarMod][Network][Login] Invalid login for Client " + client.getNetworkId() + " (" + Login.LoginCode.getById(returnCode).getMessage() + ")");
+        } else {
+            System.out.println("[StarMod][Network][Login] Logging in Client " + client.getNetworkId() + " with protected username: " + playerName);
+        }
+        Login returnLogin = new Login(cmd.getHeader(), playerName, cmd.getVersion(), cmd.getAddress(), cmd.getLoginCode(), returnCode, cmd.getUserAgent(), client.getNetworkId());
+        client.send(returnLogin);
+        client.onLogin(returnLogin);
+    }
 
 }
